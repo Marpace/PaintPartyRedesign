d73f1aa716d2d60929ac30a17bd81678e944b94d
Marpace <77944842+Marpace@users.noreply.github.com>
2021-03-21 14:38:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,71 +1,112 @@
-const item1 = document.querySelector('.item1');
-const item2 = document.querySelector('.item2');
-const item3 = document.querySelector('.item3');
-const triggerNext = document.querySelector('.triggerNext');
-const triggerPrevious = document.querySelector('.triggerPrevious');
 
-const items = [item1, item2, item3]
+const item1 = $('.item1');
+const item2 = $('.item2');
+const item3 = $('.item3');
+
+const items = [item1, item2, item3];
 
 const rotateForward = function(){
+  
+
     items.forEach(item => {
-        if(item.classList.contains('toFront')){
-            item.classList.add('toBack');
-            item.classList.remove('toFront')
-        } else if (item.classList.contains('toMiddle')){
-            item.classList.add('toFront')
-            item.classList.remove('toMiddle')
+        if(item.hasClass('toFront')){
+            item.animate({
+                top: "160",
+                opacity: 0
+            }, 300, function(){
+                item.css("top", "");
+            });
+            item.delay(300).animate({
+                opacity: 1
+            });
+            setTimeout(function(){
+                item.addClass('toBack');
+                item.removeClass('toFront');
+            }, 300);
+            
+        } else if (item.hasClass('toMiddle')){
+            setTimeout(function(){
+                item.addClass('toFront');
+            item.removeClass('toMiddle');
+            }, 100);
+            
         } else {
-            item.classList.add('toMiddle')
-            item.classList.remove('toBack')
+            setTimeout(function(){
+                item.addClass('toMiddle');
+            item.removeClass('toBack');
+            }, 100);
         }
     });   
 };
 
 const rotateBackwards = function(){
+  
     items.forEach(item => {
-        if(item.classList.contains('toFront')){
-            item.classList.add('toMiddle');
-            item.classList.remove('toFront')
-        } else if (item.classList.contains('toMiddle')){
-            item.classList.add('toBack')
-            item.classList.remove('toMiddle')
+        if(item.hasClass('toFront')){
+            setTimeout(function(){
+                item.addClass('toMiddle');
+            item.removeClass('toFront');
+            }, 100);
+            
+        } else if (item.hasClass('toMiddle')){
+            setTimeout(function(){
+                item.addClass('toBack');
+            item.removeClass('toMiddle');
+            }, 100);
+            
         } else {
-            item.classList.add('toFront')
-            item.classList.remove('toBack')
+            item.animate({
+                top: "160",
+                opacity: 0
+            }, 300, function(){
+                item.css("top", "");
+            });
+            item.delay(300).animate({
+                opacity: 1
+            });
+
+            setTimeout(function(){
+                item.addClass('toFront');
+            item.removeClass('toBack');
+            }, 300);
         }
-    });     
+    });   
 };
 
-triggerNext.addEventListener('click', function(){
+
+
+
+$('.triggerNext').click(function(){
     rotateForward();
-});
-triggerPrevious.addEventListener('click', function(){
+});  
+
+$('.triggerPrevious').click(function(){
     rotateBackwards();
 });
 
 
-const selector1 = document.getElementById('selector-1');
-const selector2 = document.getElementById('selector-2');
-const selector3 = document.getElementById('selector-3');
+const selector1 = $('#selector-1')
+const selector2 = $('#selector-2')
+const selector3 = $('#selector-3')
 
-selector1.addEventListener('click', function(){
-    if(item1.classList.contains('toMiddle')){
+selector1.click(function(){
+    if(item1.hasClass('toMiddle')){
         rotateForward();
-    } else if(item1.classList.contains('toBack')){
+    } else if(item1.hasClass('toBack')){
         rotateBackwards();
     }
 });
-selector2.addEventListener('click', function(){
-    if(item2.classList.contains('toMiddle')){
+selector2.click(function(){
+    if(item2.hasClass('toMiddle')){
         rotateForward();
-    } else if (item2.classList.contains('toBack')){
+    } else if(item2.hasClass('toBack')){
         rotateBackwards();
     }
 });
-selector3.addEventListener('click', function(){
-    if(item3.classList.contains('toMiddle')){
+selector3.click(function(){
+    if(item3.hasClass('toMiddle')){
         rotateForward();
-    } else if (item3.classList.contains('toBack')){
+    } else if(item3.hasClass('toBack')){
         rotateBackwards();
     }
 });
