0ee503d6c9aa57d0ccf1ffe6ee98b508a59ccbcd
Marpace <77944842+Marpace@users.noreply.github.com>
2021-03-24 22:35:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 
+
 const item1 = $('.item1');
 const item2 = $('.item2');
 const item3 = $('.item3');
@@ -76,46 +77,122 @@ const rotateBackwards = function(){
 
 
 
-$('.triggerNext').click(function(){
-    rotateForward();
-});  
-
-$('.triggerPrevious').click(function(){
-    rotateBackwards();
-});
 
 
 const selector1 = $('#selector-1')
 const selector2 = $('#selector-2')
 const selector3 = $('#selector-3')
 
+const selectors = [selector1, selector2, selector3]
+
+const cardsText1 = $('#cards-text-1');
+const cardsText2 = $('#cards-text-2');
+const cardsText3 = $('#cards-text-3');
+
+
+
+
 selector1.click(function(){
-    if(item1.hasClass('toMiddle')){
+    cardsText1.addClass('textFade');
+    cardsText1.removeClass('textHidden');
+    cardsText2.addClass('textHidden')
+    cardsText2.removeClass('textFade');
+    cardsText3.addClass('textHidden');
+    cardsText3.removeClass('textFade');
+    if(item1.hasClass('to`Middle')){
         rotateForward();
     } else if(item1.hasClass('toBack')){
         rotateBackwards();
-    }
+    };
+    if(!selector1.hasClass('selector-active')){
+        selector1.addClass('selector-active')
+        selector2.removeClass('selector-active');
+        selector3.removeClass('selector-active');
+    };
+    
+    
 });
 selector2.click(function(){
+    cardsText2.addClass('textFade');
+    cardsText2.removeClass('textHidden')
+    cardsText3.addClass('textHidden');
+    cardsText3.removeClass('textFade');
+    cardsText1.addClass('textHidden');
+    cardsText1.removeClass('textFade');
     if(item2.hasClass('toMiddle')){
         rotateForward();
     } else if(item2.hasClass('toBack')){
         rotateBackwards();
-    }
+    };
+
+    if(!selector2.hasClass('selector-active')){
+        selector2.addClass('selector-active')
+        selector1.removeClass('selector-active');
+        selector3.removeClass('selector-active');
+    };
+    
 });
 selector3.click(function(){
+    cardsText3.addClass('textFade');
+    cardsText3.removeClass('textHidden')
+    cardsText2.addClass('textHidden');
+    cardsText2.removeClass('textFade');
+    cardsText1.addClass('textHidden');
+    cardsText1.removeClass('textFade');
     if(item3.hasClass('toMiddle')){
         rotateForward();
     } else if(item3.hasClass('toBack')){
         rotateBackwards();
-    }
+    };
+
+    if(!selector3.hasClass('selector-active')){
+        selector3.addClass('selector-active')
+        selector2.removeClass('selector-active');
+        selector1.removeClass('selector-active');
+    };
+   
 });
+
+
+
+const cardsOptions = {
+    rootMargin: "0px",
+    threshold: .6
+};
+
+const cardsCallback = (entries, observer) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            item1.animate({
+                opacity: "1"
+            }, 400);
+            item2.delay(200).animate({
+                opacity: "1"
+            }, 400);
+            item3.delay(400).animate({
+                opacity: "1"
+            }, 400);
+            item1.addClass('toFront') 
+            item2.addClass('toMiddle')
+            item3.addClass('toBack')
+
+            observer.unobserve(cardsTarget);
+        }
+        
+    });
+};
+
+const cardsTarget = document.querySelector('.cards');
+
+const observer = new IntersectionObserver(cardsCallback, cardsOptions);
+
+observer.observe(cardsTarget);
+
 
 const toggler = $('#navbar-toggler');
 const navList = $('nav ul');
 
 toggler.click(() => {
     navList.slideToggle();
-    navList.css("display", "block");
+    // navList.css("display", "block");
 });
-
